dc47202b44c58d50d99cdf1e9a70e5617cd232dc
Athifapattekha076 <athifashaik902@gmail.com>
2024-08-24 14:15:44 +0530

    Created Sidebar Successfully

--- recipe-app/src/components/Navbar.js
+++ recipe-app/src/components/Navbar.js
@@ -1,6 +1,8 @@
+ import {useState } from "react"
  function Navbar(){
+   const [showSidebar , setShowSidebar] = useState(false)
     return(
-       <div className="navbar"> 
+       
        <div className="navbar container">
        <a href="#!" className="logo">F<span>oo</span>diesHub</a>
        <div className="nav-links">
@@ -8,9 +10,14 @@
          <a href="#!">Recipes</a>
          <a href="#!">Settings</a>
        </div>
+       <div onClick={()=> setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
+       <div className="bar"></div>
+       <div className="bar"></div>
+       <div className="bar"></div>
+       </div>
        </div>
        
-       </div>
+       
     );
 }
 
