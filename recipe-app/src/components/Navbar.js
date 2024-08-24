359a65b7af2b4990780992d4f10629d8aad596d3
Athifapattekha076 <athifashaik902@gmail.com>
2024-08-24 13:14:04 +0530

    Navbar is created successfully..

---
+++ recipe-app/src/components/Navbar.js
@@ -0,0 +1,17 @@
+ function Navbar(){
+    return(
+       <div className="navbar"> 
+       <div className="navbar container">
+       <a href="#!" className="logo">F<span>oo</span>diesHub</a>
+       <div className="nav-links">
+         <a href="#!">Home</a>
+         <a href="#!">Recipes</a>
+         <a href="#!">Settings</a>
+       </div>
+       </div>
+       
+       </div>
+    );
+}
+
+export default Navbar;
